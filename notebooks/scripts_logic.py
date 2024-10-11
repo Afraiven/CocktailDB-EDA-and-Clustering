a468e8e7f25cb480ac2bb6849516129bc5eb36ea
Afraiven <filip.antoniak99@gmail.com>
2024-10-11 15:22:59 +0200

    Refactor code for improved performance and update data analysis visualizations

--- notebooks/scripts_logic.py
+++ notebooks/scripts_logic.py
@@ -17,36 +17,36 @@ def show_tags(data):
 
 
 def analyze_IBA(data):
-    # oficjalne IBA
+    # official IBA
     with open("../data/official_IBA.txt", "r", encoding="utf-8") as file:
         official_IBA = file.read().splitlines()
 
     i = 0
-    # weryfikuję czy tagi IBA sie zgadzają 
+    # verify official IBA
     for _, row in data.iterrows():
         if type(row["tags"]) == list:
             if row["tags"].count("IBA") > 0:
                 if row["name"] not in official_IBA:
-                    print("Drinki usunięte z IBA:", row["name"])
+                    print("Drinks that has been removed from IBA:", row["name"])
             elif row["name"] in official_IBA:
                 print(row["name"], row["tags"])
                 
 
-    print("Anomalie: ", i)
+    print("Anomalies: ", i)
 
 
 def generate_hist_and_pie(data, column):
     plt.figure(figsize=(10, 5))
     data[column].value_counts().plot(kind='bar')
-    plt.title(f'Histogram kolumny - {column}')
+    plt.title(f'Hist of - {column}')
     plt.xlabel(column)
-    plt.ylabel('Ilość drinków')
+    plt.ylabel('Num of drinks')
     plt.show()
 
     plt.figure(figsize=(8, 8))
     category_counts = data[column].dropna().value_counts()
     plt.pie(category_counts, labels=category_counts.index, autopct='%1.1f%%')
-    plt.title(f'Pie Chart kolumny - {column}')
+    plt.title(f'Pie Chart - {column}')
     plt.ylabel('')
     plt.show()
 
@@ -83,17 +83,17 @@ def tags_contain_IBA(data):
             tags += 1
             if "IBA" in i:
                 IBA += 1
-    print(f"Drinków z tagami: {tags}")
-    print(f"Drinków z tagiem IBA: {IBA}")
-    print(f"Drinków z tagami ale bez IBA: {tags - IBA}")
+    print(f"Drinks with tags: {tags}")
+    print(f"Drinks with IBA tag: {IBA}")
+    print(f"Drinks with tags but without IBA: {tags - IBA}")
     # pie chart
     plt.figure(figsize=(8, 8))
     plt.pie([IBA, tags - IBA], labels=["IBA", "Tags"], autopct='%1.1f%%')
-    plt.title('Pie Chart tagów z lub bez IBA')
+    plt.title('Pie Chart of drinks with tags but without IBA')
     plt.show()
 
 def analyze_instructions(data):
-    # histogram kolumny - długość słów w instructions
+    # histogram of the column - word length in "instructions"
     instructions = []
     for i in data.instructions:
         if i:
@@ -104,9 +104,9 @@ def analyze_instructions(data):
 
     plt.figure(figsize=(18, 8))
     instructions_counts.plot(kind='bar')
-    plt.title('Histogram długości słów w instrukcjach')
-    plt.xlabel('Długość słów')
-    plt.ylabel('Częstotliwość')
+    plt.title('Histogram word length in "instructions"')
+    plt.xlabel('Word length')
+    plt.ylabel('Frequency')
     plt.show()
 
     bins = range(0, instructions.max() + 5, 5)
@@ -116,16 +116,16 @@ def analyze_instructions(data):
 
     plt.figure(figsize=(15, 8))
     instructions_counts.plot(kind='bar')
-    plt.title('Histogram długości słów w instrukcjach - bins = 5')
-    plt.xlabel('Przedziały długości słów')
-    plt.ylabel('Częstotliwość')
+    plt.title('Histogram word length in "instructions" - bins = 5')
+    plt.xlabel('Word length intervals')
+    plt.ylabel('Frequency')
     plt.show()
 
     print(instructions_counts)
 
     plt.figure(figsize=(18, 8))
     instructions_counts.plot(kind='pie', autopct='%1.1f%%')
-    plt.title('Wykres kołowy długości słów w instrukcjach')
+    plt.title('Pie chart of word lengths in instructions')
     plt.ylabel('')  
     plt.show()
 
@@ -145,12 +145,12 @@ def drinks_with_ingredient_in_name(data):
                 if j in i.lower():
                     ingredients_in_name += 1
                     break
-    print(f"Drinków, których składniki znajdują się w nazwie: {ingredients_in_name}")
-    print(f"Drinków, których składniki nie znajdują się w nazwie: {len(data) - ingredients_in_name}")
+    print(f"Drinks whose ingredients are in the name: {ingredients_in_name}")
+    print(f"Drinks whose ingredients are NOT in the name: {len(data) - ingredients_in_name}")
 
     plt.figure(figsize=(8, 8))
-    plt.pie([ingredients_in_name, len(data) - ingredients_in_name], labels=["Składnik w nazwie", "Brak składników w nazwie"], autopct='%1.1f%%')
-    plt.title('Pie Chart składników w nazwie')
+    plt.pie([ingredients_in_name, len(data) - ingredients_in_name], labels=["Ingredient in name", "No ingredient in name"], autopct='%1.1f%%')
+    plt.title('Pie Chart of Ingredients in Name')
     plt.show()
 
 
